3796a8ee580039b8c509b30fca864f31f6e21a4c
Jenny Kennedy <kennedyjenny27@gmail.com>
2022-03-13 17:14:20 -0700

    Done with assignment

---
+++ index.js
@@ -0,0 +1,40 @@
+function shout(string) {}
+function shout(string) {
+    return string;
+  }
+  "Hello!".toUpperCase(); // 'HELLO!'
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string) {}
+  function whisper(string) {
+    return string
+  }
+  "Hello".toLowerCase(); // 'hello!'
+  function whisper(string) {
+      return string.toLowerCase();
+  }
+
+  function logShout (hello) {
+      console.log(hello.toUpperCase( ));
+  }
+
+  function logWhisper (hello) {
+      console.log(hello.toLowerCase( ));
+  }
+
+  
+
+function sayHiToHeadphonedRoommate(string) {
+    
+    if (string.toUpperCase() === string) {
+        return "YES INDEED!" 
+    } else if (string === "Let's have dinner together!") {
+        return "I would love to!"
+    }
+    
+    return "I can\'t hear you!"
+}
+
+
